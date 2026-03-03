078841c09c393f30c2d89306f865904caf306254
Mukesh <mukesh2305b@gmail.com>
2026-03-03 22:30:29 +0530

    Create dark.js

---
+++ theme/dark.js
@@ -0,0 +1,10 @@
+export const dark = {
+    background: "hsl(240 10% 3.9%)",
+    foreground: "hsl(0 0% 98%)",
+    caret: "hsl(210 20% 98%)",
+    selection: "hsl(215 27.9% 16.9%)",
+    selectionMatch: "hsl(215 27.9% 16.9%)",
+    lineHighlight: "transparent",
+    gutterBackground: "hsl(240 10% 3.9%)",
+    gutterForeground: "hsl(0 0% 98%)",
+};
